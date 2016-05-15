5193b4b4f2c4a11b3da5ccbdd33269d28dae0939
Stephane LOPES-NEVES <stephane@lopes-neves.fr>
2016-05-15 19:24:58 +0200

    Customize shape

--- square_draw.py
+++ square_draw.py
@@ -2,12 +2,17 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("grey")
+    
 
     stephane = turtle.Turtle()
     turn_times = 4
     turn_count = 0
+
+    stephane.shape("turtle")
+    shape_colors = ["blue", "green", "yellow", "black"]
     while (turn_count < turn_times):
+        stephane.color(shape_colors[turn_count])
         stephane.forward(100)
         stephane.right(90)
         turn_count = turn_count + 1
